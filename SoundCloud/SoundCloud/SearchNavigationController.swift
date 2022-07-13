1271226a26f4bb9490e7b472ba3e232fb11fb6a9
김규철 <rbcjf122@gmail.com>
2022-07-13 18:10:00 +0900

    create SearchNavigationController

---
+++ SoundCloud/SoundCloud/SearchNavigationController.swift
@@ -0,0 +1,34 @@
+//
+//  SearchNavigationController.swift
+//  SoundCloud
+//
+//  Created by qualson1 on 2022/07/13.
+//
+
+import UIKit
+
+class SearchNavigationController: UINavigationController {
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
