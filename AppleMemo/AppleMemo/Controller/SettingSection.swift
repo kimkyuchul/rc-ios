7eabaf4b1ad5b1a67c2ef2c38af17802e46fdc72
김규철 <rbcjf122@gmail.com>
2022-08-05 23:48:36 +0900

    Edit : Section

--- AppleMemo/AppleMemo/Controller/SettingSection.swift
+++ AppleMemo/AppleMemo/Controller/SettingSection.swift
@@ -5,20 +5,32 @@
 //  Created by qualson1 on 2022/08/04.
 //
 
+//https://velog.io/@din0121/Swift-Table-View-Multi-Section
+
+
 import Foundation
 import UIKit
 
-enum SettingSection {
-    case option1([optionOne])
-    case option2([optionTwo])
+enum CellType: String {
+    case detailTitle
+    case rightDetail
 }
 
-struct optionOne {
+struct SettingItem {
     let title: String?
     let iconImage: UIImage?
+    let type: CellType
 }
 
-struct optionTwo {
-    let title: String?
-    let iconImage: UIImage?
+struct SettingSection {
+    let items: [SettingItem]
+    
+    func generateData() -> [SettingSection] {
+        
+        return [
+            SettingSection(items: [SettingItem(title: "", iconImage: <#T##UIImage?#>, type: <#T##CellType#>)])
+        ]
+        
+    }
+    
 }
